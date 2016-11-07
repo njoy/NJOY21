27898d12a9f283aaebc82bcd32cda102af532850
Austin McCartney <amccartney@lanl.gov>
2016-11-07 09:15:59 -0700

    initial commit

---
+++ src/njoy21.hpp
@@ -0,0 +1,19 @@
+#ifndef NJOY21
+#define NJOY21
+
+#include <sstream>
+
+#include "dimwits.hpp"
+#include "utility.hpp"
+
+namespace njoy{
+namespace njoy21{
+
+#include "njoy21/argument.hpp"
+#include "njoy21/input.hpp"
+#include "njoy21/MODER.hpp"
+
+}
+}
+
+#endif
