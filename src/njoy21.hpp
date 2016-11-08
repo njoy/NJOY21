7b73d0894df03a31f7980c0b16958dbde6d316cf
Austin McCartney <amccartney@lanl.gov>
2016-11-08 11:05:22 -0700

    submodules

--- src/njoy21.hpp
+++ src/njoy21.hpp
@@ -4,16 +4,23 @@
 #include <sstream>
 
 #include "dimwits.hpp"
+#include "Log.hpp"
 #include "utility.hpp"
 
 namespace njoy{
 namespace njoy21{
 
+template< typename T >
+using optional = std::experimental::optional< T >;
+
 #include "njoy21/argument.hpp"
 #include "njoy21/input.hpp"
-#include "njoy21/MODER.hpp"
+
+struct CommandLine;
 
 }
 }
+
+#include "njoy21/CommandLine.hpp"
 
 #endif
