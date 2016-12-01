2072eccbed42d83ea9abb34800cfaacb6aab9caf
Austin McCartney <amccartney@lanl.gov>
2016-12-01 13:46:16 -0700

    variadic template functions are magic

--- src/njoy21/input/argument/primitive.hpp
+++ src/njoy21/input/argument/primitive.hpp
@@ -20,26 +20,28 @@ Istream& operator>>( Istream& is, std::vector< Value >& v ){
 template< typename T >
 struct Type {
   using Data_t = T;
-  template< typename Istream >
-  static bool read( Istream& is, T& i );
+  template< typename Istream, typename... Args >
+  static bool read( Istream& is, T& i, Args&&... );
 };
 
 template< typename T >
-template< typename Istream >
-bool Type<T>::read( Istream& is, T& i ){ is >> i; return true; }
+template< typename Istream, typename... Args >
+bool Type<T>::read( Istream& is, T& i, Args&&... ){
+  is >> i; return true;
+}
 
 template<>
-template< typename Istream >
-inline bool Type< std::string >::read( Istream& is, std::string& i ){
+template< typename Istream, typename... Args >
+inline bool Type< std::string >::read( Istream& is, std::string& i, Args&&... ){
   is >> std::quoted( i, '\'', char(33) ); return true;
 }
 
 template< typename Core >
 struct Required : protected Core {
   using Data_t = typename Core::Data_t;
-  template< typename Istream >
-  static bool read( Istream& is, Data_t& d ){
-    Core::read( is, d );
+  template< typename Istream, typename... Args >
+  static bool read( Istream& is, Data_t& d, Args&&... args ){
+    Core::read( is, d, std::forward<Args>(args)... );
     if ( is.fail() ){
       is.eof() ? throw std::ios_base::failure("") : throw std::domain_error("");
     }
@@ -50,10 +52,10 @@ struct Required : protected Core {
 template< typename Core >
 struct Optional : protected Core {
   using Data_t = typename Core::Data_t;
-  template< typename Istream >
-  static bool read( Istream& is, Data_t& d ){
+  template< typename Istream, typename... Args >
+  static bool read( Istream& is, Data_t& d , Args&&... args ){
     is.exceptions( std::ios_base::goodbit );
-    Core::read( is, d );
+    Core::read( is, d, std::forward<Args>(args)... );
     return is.fail() ? is.eof() ? false : throw std::domain_error("") : true;
   }
 };
@@ -64,7 +66,7 @@ struct Discriminating : protected Core {
   using Data_t = typename Core::Data_t;
   template< typename Istream, typename... Args >
   static bool read( Istream& is, Data_t& d, Args&&... args ){
-    if ( Core::read( is, d ) ){
+    if ( Core::read( is, d, std::forward<Args>(args)... ) ){
       if ( not Child::verify( d, std::forward<Args>(args)... ) ){
 	throw std::domain_error("");
       }
@@ -80,7 +82,7 @@ struct Defaulted : protected Core {
   using Data_t = typename Core::Data_t;
   template< typename Istream, typename... Args >
   static bool read( Istream& is, Data_t& d, Args&&... args ){
-    if ( not Optional<Core>::read( is, d, args... ) ){
+    if ( not Optional<Core>::read( is, d, std::forward<Args>(args)... ) ){
       d = Child::defaultValue( std::forward<Args>(args)... );
       return false;
     }
