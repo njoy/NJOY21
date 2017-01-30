8543029ef29f1d5a5d65244b94ec41ded1172827
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2017-01-30 11:43:33 -0700

    I dont' know what I'm doing.

--- .travis/build.sh
+++ .travis/build.sh
@@ -11,23 +11,16 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     export appended_flags=$appended_flags" -isystem /usr/include/c++/v1/"
     export CUSTOM=("-D no_link_time_optimization=TRUE")
   else
-#   sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 90 \
-#        --slave /usr/bin/g++ g++ /usr/bin/g++-6 \
-#        --slave /usr/bin/gfortran gfortran /usr/bin/gfortran-6 \
-#        --slave /usr/bin/gcov gcov /usr/bin/gcov-6 \
-#        --slave /usr/bin/gcc-ar ar /usr/bin/gcc-ar-6 \
-#        --slave /usr/bin/gcc-nm nm /usr/bin/gcc-nm-6 \
-#        --slave /usr/bin/gcc-ranlib ranlib /usr/bin/gcc-ranlib-6
-#   sudo update-alternatives --config gcc
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 90 \
+         --slave /usr/bin/g++ g++ /usr/bin/g++-6 \
+         --slave /usr/bin/gfortran gfortran /usr/bin/gfortran-6 \
+         --slave /usr/bin/gcov gcov /usr/bin/gcov-6 \
+         --slave /usr/bin/gcc-ar ar /usr/bin/gcc-ar-6 \
+         --slave /usr/bin/gcc-nm nm /usr/bin/gcc-nm-6 \
+         --slave /usr/bin/gcc-ranlib ranlib /usr/bin/gcc-ranlib-6
+    sudo update-alternatives --config gcc
     export appended_flags=$appended_flags" -Wno-error=subobject-linkage -Wno-subobject-linkage"
-#   export CUSTOM=('-D CMAKE_AR=/usr/bin/gcc-ar' '-D CMAKE_NM=/usr/bin/gcc-nm' '-D CMAKE_RANLIB=/usr/bin/gcc-ranlib')
-    ln -s /usr/bin/gcc-6 /usr/local/bin/gcc
-    ln -s /usr/bin/g++-6 /usr/local/bin/g++
-    ln -s /usr/bin/gfortran /usr/local/bin/gfortran
-    export CC=/usr/bin/gcc-6
-    export CXX=/usr/bin/g++-6
-    export FC=/usr/bin/gfortran
-    gcc -v && g++ -v && gofrtran -v
+    export CUSTOM=('-D CMAKE_AR=/usr/bin/gcc-ar' '-D CMAKE_NM=/usr/bin/gcc-nm' '-D CMAKE_RANLIB=/usr/bin/gcc-ranlib')
   fi;
 fi
 
