146c2d74abe00e860086e9f7b9892884adf72bb3
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2017-04-05 15:13:38 -0600

    Fixing a little more about GROUPR Card3.

--- src/njoy21/input/GROUPR.hpp
+++ src/njoy21/input/GROUPR.hpp
@@ -2,13 +2,17 @@ class GROUPR {
 public:
   /* nested types */
   #include "njoy21/input/GROUPR/Card1.hpp"
+//#include "njoy21/input/GROUPR/Card2.hpp"
+  #include "njoy21/input/GROUPR/Card3.hpp"
 
   Card1 card1;
 
   template< typename Istream >
   GROUPR( Istream& is )
   try:
-    card1( is )
+    card1( is ),
+//  card2( is ),
+    card3( is )
   {
   }
   catch( std::exception& e ){
