be62067c31995b165cf276b5797ad14e28478b64
Austin McCartney <amccartney@lanl.gov>
2017-05-29 16:13:58 -0600

    updated travis

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,35 +2,34 @@
 set -x
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 90 --slave /usr/bin/g++ g++ /usr/bin/g++-6 --slave /usr/bin/gcov gcov /usr/bin/gcov-6
+  sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 90 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-3.8
+  sudo update-alternatives --config gcc
+  sudo update-alternatives --config clang
   if [ "$CXX" = "clang++" ]; then
-    sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 90 \
-	 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-3.8 \
-	 --slave /usr/bin/gfortran gfortran /usr/bin/gfortran-6
-    sudo update-alternatives --config clang
-    export appended_flags=$appended_flags"-stdlib=libstdc++"
     export PATH=/usr/bin:$PATH
-    export CUSTOM=("-D no_link_time_optimization=TRUE")
+    export CUSTOM='-D link_time_optimization=OFF'
   else
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 90 \
-         --slave /usr/bin/g++ g++ /usr/bin/g++-6 \
-         --slave /usr/bin/gfortran gfortran /usr/bin/gfortran-6 \
-         --slave /usr/bin/gcov gcov /usr/bin/gcov-6 \
-         --slave /usr/bin/gcc-ar ar /usr/bin/gcc-ar-6 \
-         --slave /usr/bin/gcc-nm nm /usr/bin/gcc-nm-6 \
-         --slave /usr/bin/gcc-ranlib ranlib /usr/bin/gcc-ranlib-6
-    sudo update-alternatives --config gcc
-    export CUSTOM=('-D CMAKE_AR=/usr/bin/gcc-ar' '-D CMAKE_NM=/usr/bin/gcc-nm' '-D CMAKE_RANLIB=/usr/bin/gcc-ranlib')
+    export CUSTOM='-D link_time_optimization=ON'
+  fi;
+  if [ "$build_type" = "coverage" ]; then
+    export build_type=DEBUG
+    export coverage=true
+    export CUSTOM="$CUSTOM -D coverage=ON"
+  else
+    export coverage=false
   fi;
 fi
 
 mkdir build
 cd build
-cmake ${CUSTOM[@]}\
-      -D build_type=$build_type \
+cmake -D CMAKE_BUILD_TYPE=$build_type \
       -D static_libraries=$static_libraries \
-      -D NJOY21_appended_flags="$appended_flags" ..
-make -j2
+      -D NJOY21_appended_flags="$appended_flags" \
+      $CUSTOM ..
+make VERBOSE=1 -j2
 export COMPILATION_FAILURE=$?
+
 if [ $COMPILATION_FAILURE -ne 0 ];
 then
   exit 1
@@ -42,8 +41,7 @@ if [ $TEST_FAILURE -ne 0 ];
 then
     exit 1
 fi
-if [ "$build_type" = "coverage" ]
-then
+if $coverage; then
   pip install --user cpp-coveralls
   echo "loading coverage information"
   coveralls  --exclude-pattern "/usr/include/.*|.*/CMakeFiles/.*|.*subprojects.*|.*dependencies.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp'
