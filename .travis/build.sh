9e5d62750e7cb26ddb2bc6192e9f790b9f181ef3
Austin McCartney <amccartney@lanl.gov>
2017-05-29 17:34:06 -0600

    path mistake in build script

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   sudo update-alternatives \
     --install /usr/bin/gcc gcc /usr/bin/gcc-6 90 \
     --slave /usr/bin/g++ g++ /usr/bin/g++-6 \
-    --slave /usr/bin/gfortran gfortran usr/bin/gfortran-6 \
+    --slave /usr/bin/gfortran gfortran /usr/bin/gfortran-6 \
     --slave /usr/bin/gcov gcov /usr/bin/gcov-6
   sudo update-alternatives \
     --install /usr/bin/clang clang /usr/bin/clang-3.8 90 \
