7808df9cb5da7f5a703b5fc211714d2a4f3b4eda
Austin McCartney <amccartney@lanl.gov>
2017-06-04 19:01:32 -0600

    reduced verbosity

--- .travis/build.sh
+++ .travis/build.sh
@@ -33,23 +33,43 @@ cd build
 cmake -D CMAKE_BUILD_TYPE=$build_type \
       -D static_libraries=$static_libraries \
       -D NJOY21_appended_flags="$appended_flags" \
-      $CUSTOM ..
-make VERBOSE=1 -j2
+      $CUSTOM .. &> configuration.txt
+export CONFIGURATION_FAILURE=$?
+
+if [ $CONFIGURATION_FAILURE -ne 0 ];
+then
+  echo "failed while configuring"
+  cat configuration.txt
+  exit 1
+fi
+
+make VERBOSE=1 -j2 &> compilation.txt
 export COMPILATION_FAILURE=$?
 
 if [ $COMPILATION_FAILURE -ne 0 ];
 then
+  echo "failed while compiling"
+  cat compilation.txt  
   exit 1
 fi
 
-ctest --output-on-failure -j2
+ctest --output-on-failure -j2 &> testing.txt
 export TEST_FAILURE=$?
 if [ $TEST_FAILURE -ne 0 ];
 then
+    echo "failed while testing"
+    cat testing.txt  
     exit 1
 fi
+
 if $coverage; then
-  pip install --user cpp-coveralls
-  echo "loading coverage information"
-  coveralls  --exclude-pattern "/usr/include/.*|.*/CMakeFiles/.*|.*subprojects.*|.*dependencies.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp'
+  pip install --user cpp-coveralls &> coverage_upload.txt
+  echo "failed while loading coverage information"
+  coveralls  --exclude-pattern "/usr/include/.*|.*/CMakeFiles/.*|.*subprojects.*|.*dependencies.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp' &>> coverage_upload.txt
+  if [ $? -ne 0 ];
+  then
+     echo "failed while coverage report!"
+     cat coverage_upload.txt
+     exit 1
+  fi
 fi
