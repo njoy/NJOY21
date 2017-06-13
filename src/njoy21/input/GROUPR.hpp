7437502b0130a3e8ebca0778c034f050d18ba837
Travis CI <travis@travis-ci.org>
2017-06-12 19:52:08 -0700

    Working on GROUPR card sequence.

--- src/njoy21/input/GROUPR.hpp
+++ src/njoy21/input/GROUPR.hpp
@@ -22,16 +22,14 @@ public:
   Card3 card3;
   Card4 card4;
   Card5 card5;
-  Card6a card6a;
-  Card6b card6b;
-  Card7a card7a;
-  Card7b card7b;
-  Card8a card8a;
-  Card8b card8b;
-  Card8c card8c;
-  Card8d card8d;
-  Card9 card9;
-  Card10 card10;
+  optional< std::tuple< Card6a, Card6b > > arbitraryNeutronStructureCards;
+  optional< std::tuple< Card7a, Card7b > > arbitraryGammaStructureCards;
+  optional< Card8a > card8a;
+  optional< Card8b > card8b;
+  optional< Card8c > card8c;
+  optional< Card8d > card8d;
+  optional< std::vector< Card9 > > card9List;
+  optional< std::vector< Card10 > > card10List;
 
   template< typename Istream >
   GROUPR( Istream& is )
@@ -39,19 +37,54 @@ public:
     card1( is ),
     card2( is ),
     card3( is ),
-    card4( is ),
-    card5( is ),
-    card6a( is ),
-    card6b( is ),
-    card7a( is ),
-    card7b( is ),
-    card8a( is ),
-    card8b( is ),
-//  card8c( is ),
-    card8d( is ),
-    card9( is ),
-    card10( is )
+    card4( is, card2.ntemp ),
+    card5( is, card2.nsigz )
   {
+    if( card2.ign.value == 1 ){
+      Card6a card6a( is );
+      Card6b card6b( is, card6a.ngn );
+
+      arbitraryNeutronStructureCards = std::make_tuple< Card6a, Card6b >(
+          std::move( card6a ), std::move( card6b ) );
+    }
+    if( card2.igg.value == 1 ){
+      Card7a card7a( is );
+      Card7b card7b( is, card7a.ngg );
+
+      arbitraryGammaStructureCards = std::make_tuple< Card7a, Card7b >(
+          std::move( card7a ), std::move( card7b ) );
+    }
+    if( card2.iwt.value < 0 ){
+      this->card8a = Card8a( is );
+    }
+    if( ( card2.iwt.value == 1 ) or ( card2.iwt.value == -1 ) ){
+      this->card8b = Card8b( is );
+    }
+    if( ( card2.iwt.value == 4 ) or ( card2.iwt.value == -4 ) ){
+      this->card8c = Card8c( is );
+    }
+    if( card2.iwt.value == 0 ){
+      this->card8d = Card8d( is );
+    }
+
+    Card9 card9( is );
+    if( card9.mfd.value ){
+      this->card9List = std::vector< Card9 >();
+      this->card9List->push_back( std::move( card9 ) );
+      do {
+        this->card9List->emplace_back( is );
+      } while ( this->card9List->back().mfd.value );
+    }
+
+    Card10 card10( is );
+    if( card10.matd.value ){
+      this->card10List = std::vector< Card10 >();
+      this->card10List->push_back( std::move( card10 ) );
+
+      do {
+        this->card10List->emplace_back( is );
+      } while ( this->card10List->back().matd.value );
+    }
   }
   catch( std::exception& e ){
     Log::info( "Trouble validating GROUPR input." );
--- src/njoy21/input/GROUPR/test/GROUPR.test.cpp
+++ src/njoy21/input/GROUPR/test/GROUPR.test.cpp
@@ -6,7 +6,30 @@
 
 using namespace njoy::njoy21::input;
 
+std::string sCard1( "-30 -32 0 -34 /\n" );
+std::string sCard3( "'GROUPR Card3 title for testing' /\n" );
+std::string sCard4( "293.6 /\n" );
+std::string sCard5( "1E-10 /\n" );
+
 SCENARIO( "Parsing valid GROUPR input" ){
+  WHEN( "reading in a very generic input" ){
+    iRecordStream<char> iss( std::istringstream(
+      sCard1
+      + "125 2 0 2 4 1 1 1 /"         // Card2
+      + sCard3
+      + sCard4
+      + sCard5
+      + "3 18 'MF3 MT18' /\n"         // Card 9.1
+      + "3 102 'MF3 MT102' /\n"       // Card 9.2
+      + "10 /\n"                      // Card 9.3
+      + "0 /\n"                       // Card 9.4
+      + "0 /\n"                       // Card10
+    ) );
+    THEN( "the input values can be verified" ){
+      GROUPR groupr( iss );
+    }
+  }
 }
+
 SCENARIO( "Parsing invalid GROUPR input" ){
 }
