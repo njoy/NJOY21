41025ecf1c265d0d28118ca87be8cea7f76d412e
Amelia Trainer <ameliajo@mit.edu>
2017-06-21 14:46:35 -0600

    updated cmake files for VIEWR

--- src/njoy21/input/MIXR.hpp
+++ src/njoy21/input/MIXR.hpp
@@ -1,10 +1,8 @@
 class MIXR {
 public: 
- #include "njoy21/input/MIXR/Card1.hpp"
  #include "njoy21/input/MIXR/Card4.hpp"
  #include "njoy21/input/MIXR/Card5.hpp"
  #include "njoy21/input/MIXR/Card6.hpp"
- Card1 card1;
  Card4 card4;
  Card5 card5;
  Card6 card6;
