fde0cf1e6e04e71e4d0e56c46ce027c3b6745d04
Travis CI <travis@travis-ci.org>
2017-06-28 10:20:50 -0600

    Adding additional tests to help with test coverage

--- src/njoy21/input/BROADR/Card3/Thnmax/test/Thnmax.test.cpp
+++ src/njoy21/input/BROADR/Card3/Thnmax/test/Thnmax.test.cpp
@@ -6,21 +6,28 @@
 using namespace njoy::njoy21::input;
 
 SCENARIO( "value range" ){
-  {
-    iRecordStream<char> iss( std::istringstream("   -1.0") );
-    REQUIRE( argument::extract< BROADR::Card3::Thnmax >( iss ).value
-	     == -1.0 * dimwits::electronVolt );
-  }{
-    iRecordStream<char> iss( std::istringstream("   0.0") );
-    REQUIRE( argument::extract< BROADR::Card3::Thnmax >( iss ).value
-	     == 0.0 * dimwits::electronVolt );
-  }{
-    iRecordStream<char> iss( std::istringstream("   1.0") );
-    REQUIRE( argument::extract< BROADR::Card3::Thnmax >( iss ).value
-	     == 1.0 * dimwits::electronVolt );
-  }{
-    iRecordStream<char> iss( std::istringstream(" / ") );
-    REQUIRE( argument::extract< BROADR::Card3::Thnmax >( iss ).value
-	     == 1.0 * dimwits::mega( dimwits::electronVolt ) );
+  GIVEN( "valid thnmax parameters" ){
+    THEN( "the returned class has the correct value" ){
+      for( auto thnmax : {-0.01, 0.01, 1.0, 1E6 } ){
+        iRecordStream<char> iss( 
+            std::istringstream( std::to_string( thnmax ) ) );
+
+        REQUIRE( thnmax*dimwits::electronVolt == 
+                 argument::extract< BROADR::Card3::Thnmax >( iss).value );
+      }
+    }
+  }
+  GIVEN( "no thnmax value" ){
+    THEN( "the default value is returned" ){
+      iRecordStream<char> iss( std::istringstream( "/") );
+      REQUIRE( BROADR::Card3::Thnmax::defaultValue() == 
+                argument::extract< BROADR::Card3::Thnmax >( iss ).value );
+    }
+  }
+  GIVEN( "invalid thnmax parameters" ){
+    THEN( "an exception is thrown" ){
+      iRecordStream<char> iss( std::istringstream( "0.0" ) );
+      REQUIRE_THROWS( argument::extract< BROADR::Card3::Thnmax>( iss ) );
+    }
   }
 }
