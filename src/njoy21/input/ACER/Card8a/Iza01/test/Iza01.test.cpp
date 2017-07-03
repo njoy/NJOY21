977fe717c476303fd3060538703ba578c69743a0
Travis CI <travis@travis-ci.org>
2017-07-03 14:29:57 -0600

    Adding additional checks for IZA01.

--- src/njoy21/input/ACER/Card8a/Iza01/test/Iza01.test.cpp
+++ src/njoy21/input/ACER/Card8a/Iza01/test/Iza01.test.cpp
@@ -8,10 +8,8 @@ using namespace njoy::njoy21::input;
 
 SCENARIO( "Iza01 input values", "[ACER], [Card8a], [Iza01]" ){
   GIVEN( "valid iza01 values" ){
-    std::vector<int> validIza01{ 1001, 92235 };
-
     THEN( "the returned class has the correct value" ){
-      for( auto iza01 : validIza01 ){
+      for( auto iza01 : { 1001, 92235, 100000, 199999 } ){
         iRecordStream<char> issIza01( 
             std::istringstream( std::to_string(iza01) ) );
         auto iza01Arg = argument::extract< ACER::Card8a::Iza01 >(issIza01 );
@@ -20,10 +18,14 @@ SCENARIO( "Iza01 input values", "[ACER], [Card8a], [Iza01]" ){
     }
   }
   GIVEN( "invalid iza01 values" ){
-    int iza01(-1001);
+    std::vector<int> validIza01{ 1001, 92235, 100000, 199999 };
+
     THEN( "an exception is thrown" ){
-      iRecordStream<char> issIza01( std::istringstream( std::to_string(iza01) ) );
-      REQUIRE_THROWS( argument::extract< ACER::Card8a::Iza01 >(issIza01) );
+      for( auto iza01 : {-1001, 200000 } ){
+        iRecordStream<char> issIza01( 
+            std::istringstream( std::to_string(iza01) ) );
+        REQUIRE_THROWS( argument::extract< ACER::Card8a::Iza01 >(issIza01 ) );
+      }
     }
   }
 }
