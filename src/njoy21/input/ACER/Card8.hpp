26500f0b7ac52f642d0c48a82d567677dd700d13
Travis CI <travis@travis-ci.org>
2017-07-03 09:47:25 -0600

    Moving Tempd to argument::common

--- src/njoy21/input/ACER/Card8.hpp
+++ src/njoy21/input/ACER/Card8.hpp
@@ -1,18 +1,17 @@
 class Card8 {
 public:
 
-  #include "njoy21/input/ACER/Card8/Tempd.hpp"
   #include "njoy21/input/ACER/Card8/Tname.hpp"
 
   Argument< argument::common::Matd > matd;
-  Argument< Tempd > tempd;
+  Argument< argument::common::Tempd > tempd;
   Argument< Tname > tname;
 
   template< typename Char >
   Card8( iRecordStream< Char >& is )
     try:
     matd( argument::extract< argument::common::Matd >( is ) ),
-    tempd( argument::extract< Tempd >( is ) ),
+    tempd( argument::extract< argument::common::Tempd >( is ) ),
     tname( argument::extract< Tname >( is ) )
   {
     Card::clear( is );
