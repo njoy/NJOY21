ef61be514f4d573a2fc4677799e1791ae757b0b8
nicholas-sly <sly@lanl.gov>
2018-01-05 15:17:21 -0700

    Addressed final comment.

--- src/njoy21/input/WIMSR/Card2a/Ngnd.hpp
+++ src/njoy21/input/WIMSR/Card2a/Ngnd.hpp
@@ -5,7 +5,6 @@ struct Ngnd {
     return "The ngnd parameter specifies the number of groups for the custom\n"
            "group structure being specified by the user.";
   }
-  static Value_t defaultValue(){ return 69; }
   static bool verify( const Value_t ngnd ){
     return ngnd > 0;
   }
