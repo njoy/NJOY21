f3281f31e69341abc7efa66fd4d14512e6edd6c5
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-06-18 16:43:42 -0600

    Fixing readRest.hpp so it wouldn't give compiler warning.

--- src/njoy21/input/GAMINR/Card6/src/readRest.hpp
+++ src/njoy21/input/GAMINR/Card6/src/readRest.hpp
@@ -1,18 +1,13 @@
 template< typename Istream >
 static optional< std::pair< Argument< Mtd >, Argument< Mtname > > >
 readRest( Istream& is, const int mfd ){
-  if( mfd == -1 ){
-    try{
-      Argument< Mtd > mtd( argument::extract< Mtd >( is ) );
-      Argument< Mtname > mtname( argument::extract< Mtname >( is, mfd ) );
-      
-      return std::make_pair( std::move( mtd ), std::move( mtname ) );
-    }
-    catch( ... ){}
+  Argument< Mtd > mtd( argument::extract< Mtd >( is ) );
+  Argument< Mtname > mtname( argument::extract< Mtname >( is, mfd ) );
 
+  if( mfd == -1 ){
     return std::nullopt;
   }
   else{
-    return std::nullopt;
+    return std::make_pair( std::move( mtd ), std::move( mtname ) );
   }
 }
