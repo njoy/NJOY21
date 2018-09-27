bbf87029d204fe99ba23b03c6887f18410bb6553
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-09-27 12:02:27 -0600

    Adding a few comments.

--- src/njoy21/Version.hpp
+++ src/njoy21/Version.hpp
@@ -1,7 +1,9 @@
 class Version{
 public:
   static const int majorVersion{1}; // Do not change 
+  // Change whenever new capability is implemented
   static const int minorVersion{0};
+  // Change whenever merge to master branch is done
   static const int patchVersion{0};
 
   static const std::string version;
