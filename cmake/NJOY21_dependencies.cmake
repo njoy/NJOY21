6a7672d18cd27021584ae3a9405e24bfeaa485b9
Nathan Gibson <ngibson@lanl.gov>
2020-05-20 17:56:57 -0600

    First working FetchContent rendition.

--- cmake/NJOY21_dependencies.cmake
+++ cmake/NJOY21_dependencies.cmake
@@ -16,37 +16,37 @@ cmake_policy( SET CMP0097 NEW )
 
 FetchContent_Declare( ENDFtk
     GIT_REPOSITORY  https://github.com/njoy/ENDFtk
-    GIT_TAG         origin/master
+    GIT_TAG         origin/build/fetchcontent
     GIT_SUBMODULES  "src"
     )
 
 FetchContent_Declare( dimwits
     GIT_REPOSITORY  https://github.com/njoy/DimensionalAnalysis
-    GIT_TAG         master
+    GIT_TAG         origin/build/fetchcontent
     GIT_SUBMODULES  "src"
     )
 
 FetchContent_Declare( lipservice
     GIT_REPOSITORY  https://github.com/njoy/lipservice
-    GIT_TAG         master
+    GIT_TAG         origin/build/fetchcontent
     GIT_SUBMODULES  "src"
     )
 
 FetchContent_Declare( njoy_c_bindings
     GIT_REPOSITORY  https://github.com/njoy/njoy_c_bindings
-    GIT_TAG         master
+    GIT_TAG         0f158a3f4d2ea139a2fa02948a6419a5df61e049
     GIT_SUBMODULES  "src"
     )
 
 FetchContent_Declare( tclap-adapter
     GIT_REPOSITORY  https://github.com/njoy/tclap-adapter
-    GIT_TAG         master
+    GIT_TAG         origin/master
     GIT_SUBMODULES  "src"
     )
 
 FetchContent_Declare( utility
     GIT_REPOSITORY  https://github.com/njoy/utility
-    GIT_TAG         master
+    GIT_TAG         origin/build/fetchcontent
     GIT_SUBMODULES  "src"
     )
 
@@ -61,6 +61,6 @@ FetchContent_MakeAvailable(
     lipservice
     njoy_c_bindings
     tclap-adapter
-    utilities
+    utility
     )
 
