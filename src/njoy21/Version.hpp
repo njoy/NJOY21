432c43e3d4ce90d8ea387d22af0077456b585a22
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2021-01-20 09:11:07 -0700

    Adding new test for NJOY21 version.

--- src/njoy21/Version.hpp
+++ src/njoy21/Version.hpp
@@ -4,9 +4,11 @@ public:
   // Change whenever new capability is implemented
   static constexpr int minorVersion{2};
   // Change whenever merge to master branch is done
-  static constexpr int patchVersion{0};
+  static constexpr int patchVersion{2};
 
   static std::string version(){
+    // return fmt::format( "{}.{}.{}", majorVersion, minorVersion, patchVersion );
+
     return std::to_string( majorVersion ) + "." +
            std::to_string( minorVersion ) + "." +
            std::to_string( patchVersion );
---
+++ src/njoy21/Version/test/Version.test.cpp
@@ -0,0 +1,16 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+namespace njoy21 {
+#include "njoy21/Version.hpp"
+} // namespace 
+
+// This was created in the hopes of avoiding to forget to change the Version
+// number in releases.
+// This is not a guarantee, but it should help.
+SCENARIO( "Testing NJOY21 version" ){
+  
+  CHECK( "1.2.2" == njoy21::Version::version() );
+
+} // SCENARIO
