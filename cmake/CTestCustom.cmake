d601ff9f2fbcb2d35603bf15c394c404d57cb347
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2021-03-17 12:16:13 -0600

    Ignoring Test30 as modern RECONR can't handle it.

--- cmake/CTestCustom.cmake
+++ cmake/CTestCustom.cmake
@@ -2,6 +2,9 @@
 # gets implemented
 set(CTEST_CUSTOM_TESTS_IGNORE
    $CTEST_CUSTOM_TESTS_IGNORE
+   # Can't process tape40 with RECONR because it has no MF=2/3. RECONR needs to
+   # be updated.
+   Test30
    # Poor B-10 evaluation
    Test45                        
    # Missing capability in RECONR https://github.com/njoy/RECONR/issues/14
